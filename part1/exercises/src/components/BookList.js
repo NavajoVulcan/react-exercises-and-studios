e7b7e24d30ba9f0df4e49f27cc009e2ed539761d
Rico Rose <r.andereck@gmail.com>
2023-07-23 16:43:50 -0500

    Commit1forpart1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "My Summer Reading";
+   let book1 = "https://pictures.abebooks.com/isbn/9780826412768-us.jpg";
+   let book2 = "https://bookoutlet.com/_next/image?url=https%3A%2F%2Fimages.bookoutlet.com%2Fcovers%2Flarge%2Fisbn978150%2F9781501143335-l.jpg&w=640&q=75";
+   let book3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf0ntm5OJzZdH6YZ3eS6jWvRVcOmAf5xaulTZmch2ZlAJF7mVq";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Pedagogy of the Oppressed by Paulo Freire" />
+         <img src={book2} alt="Bullshit Jobs: A Theory by David Graeber" />
+         <img src={book3} alt="Dead to the World by Charlaine Harris" />
       </div>      
    );
 }
