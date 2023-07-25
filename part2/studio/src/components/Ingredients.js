4a1787b95cd8637df6dac68f37efb197eaa5836d
Rico Rose <r.andereck@gmail.com>
2023-07-24 20:02:39 -0500

    less panicked coding, nearly finished

--- part2/studio/src/components/Ingredients.js
+++ part2/studio/src/components/Ingredients.js
@@ -1,9 +1,28 @@
+import recipedata from "./recipe.json";
+
+
 function IngredientList() {
-   return;
- }
- 
+  const ingredientsList = (
+    <div>
+      <h3>Ingredients</h3>
+      {recipedata.map((data) => {
+        return (
+          <div key={data.name}>
+            {data.ingredients.map((ingredient, index) => {
+              return <li key={index}>{ingredient}</li>;
+            })}
+      </div>
+        );
+      })}
+      </div>
+  );
+  
+  return ingredientsList;
+
+  }
+
+
  export default IngredientList;
  
 //import json file for the data
-//use a nested map to get inside the inner array
- 
+//use a nested map to get inside the inner array 
